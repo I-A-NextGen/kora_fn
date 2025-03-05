2b76d97200011199f115c2e8c41e176166d76afa
DUSHIME1212 <mdonavan33@gmail.com>
2025-03-05 13:13:11 +0200

    add framer-motion package, create NotFound component with Goback integration, and update tailwind config

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import Goback from '@/components/Goback'
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='flex items-center justify-center gap-8 flex-col text-blue-700 h-screen w-screen'>
+        <h1 className='w-1/3z text-center'>The page you are looking is not found</h1>
+        <Goback/>
+    </div>
+  )
+}
+
+export default NotFound
