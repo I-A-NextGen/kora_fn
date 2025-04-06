90497913c38b5d6f2d24cb37b577dde7f98f7258
DUSHIME1212 <mdonavan33@gmail.com>
2025-04-06 13:12:01 +0200

    fix(Logo): replace text with logo image for link rendering

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,17 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Logo = ({ link }: { link: boolean }) => {
   if (link) {
     return (
       <Link href={"/"}>
-        <p className="text-xl font-bold text-primary md:text-2xl">KORA</p>
+        <div className="relative h-10 w-32">
+          <Image
+            src={"/Umusamariyadark.png"}
+            alt="logo"
+            fill
+            className="object-contain"/>
+        </div>
       </Link>
     );
   }
